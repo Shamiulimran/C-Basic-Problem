f20e4fffe8ee704d49e0f14b5418ecd7b06fa841
shamiul-imran <mdshamiulimran@gmail.com>
2022-07-22 16:34:27 +0600

    new added git

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int i;
+            int a = 0;
+            int n = 6;
+            for (i = 1; i <= 6; i++)
+            {
+                if (6 % i == 0)
+                {
+                    a++;
+                }
+            }
+                if (a == 2)
+                {
+                    Console.WriteLine("is a     prime number");
+                }
+                else
+                {
+                    Console.WriteLine("is not a prime number");
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
