affe9cf4eef4c4102b352e12f112b409a813b3ae
shamiul-imran <mdshamiulimran@gmail.com>
2022-09-19 22:59:26 +0600

    New File Added

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int n=int.Parse(Console.ReadLine());
+            int FibnacciNumber=0;
+            int FirstNumber=0;
+            int SecondNumber=1;
+            
+            Console.WriteLine("Fibonacci Series:");
+            for (i = 0; i < n; i++)
+
+               
+
+            {
+                    FibnacciNumber = FirstNumber + SecondNumber;
+                    FirstNumber = SecondNumber;
+                    SecondNumber = FibnacciNumber;
+             
+                Console.WriteLine("{0} ",FirstNumber,SecondNumber);
+
+            }
+           
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
